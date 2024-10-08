0dc52075b2c79bcf17379ee5adebc62384e6663c
Bob <bobjacob23@yahoo.com>
2024-10-08 15:37:37 -0700

    added inquirer.prompt

---
+++ index.js
@@ -0,0 +1,9 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt ({
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee"],
+  });
+  
