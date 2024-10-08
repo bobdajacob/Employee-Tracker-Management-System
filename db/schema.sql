6bf1c4bd86ccab868c2eea866d3cd272ba897c1e
Bob <bobjacob23@yahoo.com>
2024-10-08 15:55:02 -0700

    created db folder with sql files in it

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department INTEGER,
+  FOREIGN KEY (department)
+  REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INTEGER,
+  role_id INTEGER NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+)
