ab5b6a0dd0e16e034de428ce3a1782b8af760348
Bob <bobjacob23@yahoo.com>
2024-10-10 15:27:09 -0700

    finished function addEmployee

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,19 @@
-INSERT INTO department (department_name)
+INSERT INTO department (name)
 VALUES ('Sales'),
 ('Engineering'),
 ('Finance'),
-('Legal');
+('Legal'),
+('Service');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Sales Lead', 80000, 1),
 ('Lead Engineer', 100000, 2),
 ('Account Manager', 90000, 3),
-('Lawyer', 80000, 4)
+('Lawyer', 80000, 4),
+('Customer Service', 60000, 5)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Jake', 'Myers', 2, 1),
 ('Mike', 'Smith', 1, 2),
 ('Blake', 'Woods', 4, 3),
-('John', 'Adams', 3, 4),
+('John', 'Adams', 3, 4)
--- index.js
+++ index.js
@@ -65,7 +65,6 @@ function addDepartment() {
     validate: input => input.trim() ? true : `Don't leave blank`,
   })
   .then(res => {
-    // console.log('Department Name:', res.departmentName);
     const sql = 'INSERT INTO department (name) VALUES ($1)';
     db.query(sql, [res.departmentName], (err, res) => {
       if (err) {
@@ -96,11 +95,24 @@ function addRole() {
     type: 'list',
     name: 'departmentRole',
     message: 'Which department does the role belong to?',
-    choices: ['Engineering', 'Finance', 'Legal', 'Sales', 'Service']
-  }])
-  .then(res => {
+    choices: ['Engineering',
+      'Finance', 
+      'Legal', 
+      'Sales',
+      'Service'],
+  }
+])
+  .then (res => {
+    const departmentId = {
+      'Engineering': 2,
+      'Finance': 3,
+      'Legal': 4,
+      'Sales': 1,
+      'Service': 5,
+    };
+    const transferredId = departmentId[res.departmentRole];
     const sql = `INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)`;
-    db.query(sql, [res.roleName, res.salaryRole, res.departmentRole], (err, res) => {
+    db.query(sql, [res.roleName, res.salaryRole, transferredId], (err, res) => {
       if (err) {
         console.error(`There's an error when adding the role`, err);
         return;
@@ -109,6 +121,61 @@ function addRole() {
       main();
     });
   })
+};
+
+function addEmployee() {
+  inquirer.prompt ([
+    {
+      type: 'input',
+      name: 'employeeFirstName',
+      message: `What is the employee's first name?`,
+    },
+    {
+      type: 'input',
+      name: 'employeeLastName',
+      message: `What is the employee's last name?`,
+    },
+    {
+      type: 'list',
+      name: 'employeeRole',
+      message: `What is the employee's role?`,
+      choices: ['Sales Lead', 'Lead Engineer', 'Account Manager', 'Lawyer', 'Customer Service'],
+    },
+    {
+      type: 'list',
+      name: 'employeeManager',
+      message: `Who is the employee's manager?`,
+      choices: ['None', 'Jake Myers', 'Mike Smith', 'Blake Woods', 'John Adams'],
+    },
+  ])
+  .then (res => {
+    const departmentId = {
+      'Sales Lead': 1,
+      'Lead Engineer': 2,
+      'Account Manager': 3,
+      'Lawyer': 4,
+      'Customer Service': 5,
+    };
+    const managerId = {
+      'None': null,
+      'Jake Myers': 1,
+      'Mike Smith': 2,
+      'Blake Woods': 3,
+      'John Adams': 4,
+    }
+    const transferredId = departmentId[res.employeeRole];
+    const transferredManagerId = managerId[res.employeeManager];
+
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)`
+      db.query(sql, [res.employeeFirstName, res.employeeLastName, transferredId, transferredManagerId], (err, res) => {
+        if (err) {
+          console.error(`There's an error adding the employee`, err);
+          return;
+        }
+        console.log(`Added new employee to the database!`, res);
+        main();
+      });
+    })
 };
 
 main();
